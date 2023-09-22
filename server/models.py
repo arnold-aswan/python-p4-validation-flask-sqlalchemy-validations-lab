32d8a4b030a59c96196732c788e1f2283128252e
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-09-22 22:19:42 +0300

    completed assignment

--- server/models.py
+++ server/models.py
@@ -8,12 +8,24 @@ class Author(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name= db.Column(db.String, unique=True, nullable=False)
-    phone_number = db.Column(db.String)
+    phone_number = db.Column(db.String, )
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
+    
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+           raise ValueError("Name is required")
+        elif key in name:
+            raise ValueError("Name already exists")
+        
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number) != 10:
+            raise ValueError("Phone number must be 10 digits")    
 
 class Post(db.Model):
     __tablename__ = 'posts'
@@ -30,3 +42,26 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
+
+    @validates('title')
+    def validate_title(self, key, title):
+        if not title:
+            raise ValueError("All posts must have a title")
+        elif "Won't Believe" or "Secret" or "Top" or "Guess" not in title:
+            raise ValueError("Title not clickbaity")
+        
+    @validates('content', 'summary')
+    def validate_content(self, key, valid):
+        if key == 'content':
+            if len(valid) < 250 :
+                raise ValueError("Post content must be atleast 250 chars")
+        elif key == 'summary':
+            if len(valid) > 250:
+                raise ValueError("Post summary is a maximum of 250 characters")    
+              
+            
+    @validates('category')
+    def validate_category(self, key, category):
+        if category != 'Fiction' and category != 'Non-Fiction':
+            raise ValueError("Category must be Fiction or Non-Fiction.")
+        return category    
